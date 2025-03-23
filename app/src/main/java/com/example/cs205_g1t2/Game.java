2cffb40274a3c0cda6371af025f4ae907129465f
tzejunw <tzejun.wong.2023@scis.smu.edu.sg>
2025-03-23 20:19:09 +0800

    added game loop

---
+++ app/src/main/java/com/example/cs205_g1t2/Game.java
@@ -0,0 +1,76 @@
+package com.example.cs205_g1t2;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.graphics.Paint;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+// Game manages all objects in the game, responsible for updating all states
+// and rendering all objects to the screen
+public class Game extends SurfaceView implements SurfaceHolder
+
+        .Callback{
+    private GameLoop gameLoop;
+    private Object context;
+
+    public Game(MainActivity context) {
+        super(context);
+
+        SurfaceHolder surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+
+        this.context = context;
+        gameLoop = new GameLoop(this, surfaceHolder);
+
+        setFocusable(true);
+
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+        gameLoop.startLoop();
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        drawUPS(canvas);
+        drawFPS(canvas);
+    }
+
+    public void drawUPS(Canvas canvas) {
+        String averageUPS = Double.toString(gameLoop.getAverageUPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor((Context) context, R.color.magenta);
+        paint.setColor(color);
+        paint.setTextSize(50);
+        canvas.drawText("UPS: " + averageUPS, 100, 100, paint);
+    }
+
+    public void drawFPS(Canvas canvas) {
+        String averageFPS = Double.toString(gameLoop.getAverageFPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor((Context) context, R.color.magenta);
+        paint.setColor(color);
+        paint.setTextSize(50);
+        canvas.drawText("UPS: " + averageFPS, 100, 200, paint);
+    }
+
+    public void update() {
+        // Update game state
+    }
+}
--- app/src/main/java/com/example/cs205_g1t2/MainActivity.java
+++ app/src/main/java/com/example/cs205_g1t2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cs205_g1t2;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+// MainActivity is the entry point to our app
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+//        EdgeToEdge.enable(this);
+        // Set content view to game, so that objects in the Game class can be rendered
+        // to the screen
+        setContentView(new Game(this));
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
